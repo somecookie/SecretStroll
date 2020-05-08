86ac4d2be498515d0c2d89044f13357aa3cae2c0
Ricardo Ferreira Ribeiro <ricardo.ferreiraribeiro@protonmail.com>
2020-05-08 19:13:12 +0200

    Fix IssuanceRequest

--- messages.py
+++ messages.py
@@ -2,24 +2,24 @@
 
 
 class IssuanceRequest:
-    """Client request for crendentials."""
+    """Client request for credentials."""
 
-    def __init__(self, username, attributes, com, s_s, s_t):
+    def __init__(self, commitment, R, s_s, s_t):
         """Return a new instance of an issuance request.
 
         Args:
             username (string): username of the user
             attributes (string): the attributes of the user in JSON format
-            com (petrelic.multiplicative.pairing.G1): ZK-PoK commitment to hidden attributes
+            commitment (petrelic.multiplicative.pairing.G1): ZK-PoK commitment to hidden attributes
+            R (petrelic.multiplicative.pairing.G1): commitment in the ZK-Pok
             s_s (petrelic.bn.Bn): ZK-PoK response for the secret part
             s_t (petrelic.bn.Bn): ZK-PoK response for the random t part
 
         Returns:
             IssuanceRequest: a new instance of the class
             """
-        self.username = username
-        self.attributes = attributes
-        self.com = com
+        self.com = commitment
+        self.R = R
         self.s_s = s_s
         self.s_t = s_t
 
@@ -36,4 +36,4 @@ class IssuanceResponse:
         Return:
             IssuanceResponse: a new instance of the class
         """
-        self.crendential = crendential
+        self.credential = credential

6949fa22c502bed37733683559bd7adfab7fae3b
somecookie <ricardo.ferreiraribeiro@protonmail.com>
2020-05-22 09:51:07 +0200

    Add function and picture

--- part2/utils.py
+++ part2/utils.py
@@ -2,6 +2,22 @@ import csv
 from grid import location_to_cell_id
 
 
+def n_top_loc(queries, n):
+    """
+    If you want the top location of a specific user, the queries need to be filtered prior
+    to the call to this function.
+    """
+    d = {}
+    for q in queries:
+        loc = (float(q['lat']), float(q['lon']))
+        if loc in d:
+            d[loc] += 1
+        else:
+            d[loc] = 1
+
+    return sorted(list(d), key=lambda x: x[1], reverse=True)[:n]
+
+
 def read_csv(filename):
     with open(filename) as csv_file:
         r = csv.DictReader(csv_file, delimiter=' ')

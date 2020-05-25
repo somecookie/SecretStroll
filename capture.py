dcc118d71415dfc2552459cb5469a4d6fbe5a780
Sacha Kozma <sacha.kozma@gmail.com>
2020-05-25 19:51:18 +0200

    Add conversion option

--- capture.py
+++ capture.py
@@ -5,9 +5,9 @@ import time
 import argparse
 import shutil
 
+capture_path = "captures"
 
 def capture(host,iteration):
-    capture_path = "captures"
     capture_file = pathlib.Path(capture_path)
     if capture_file.exists():
         raise RuntimeError("Remove the directory captures before running this script")
@@ -21,15 +21,28 @@ def capture(host,iteration):
             cmd = f"tcpdump host {host} -i any -w {cell_path}/run{i}.pcap".split(" ")
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
             os.system(f"python3 client.py grid -p key-client.pub -c attr.cred -r '' -t {cell_id}")
-            time.sleep(1)
             p.send_signal(subprocess.signal.SIGTERM)
-            time.sleep(1)
+
+def convert():
+    capture_file = pathlib.Path(capture_path)
+    if not capture_file.exists():
+        raise RuntimeError("Please start a capture first.")
+
+    for run_path in capture_file.glob("*/*.pcap"):
+        new_path = run_path.with_suffix('.csv')
+        cmd = "tshark -r {} -T fields -e _ws.col.Time -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e _ws.col.Length -e _ws.col.Info -E header=y -E separator=, -E quote=d > {}".format(run_path, new_path)
+        err = os.system(cmd)
+        if err != 0:
+            print("Error with file {}.".format(new_path))
+        else:
+            run_path.unlink()
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", help="Address IP of the client", type=str, default="172.18.0.2")
     parser.add_argument("--it", help="Number of iteration per cell", type=int, default=150)
+    parser.add_argument("--convert", help="In place conversion of the captures in /captures to csv files", action='store_true')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -38,4 +51,7 @@ if __name__ == "__main__":
     `python3 capture.py --ip <IP of the client> --it <number of iterations> `
     """
     args = get_args()
-    capture(args.ip, args.it)
+    if args.convert:
+        convert()
+    else:
+        capture(args.ip, args.it)

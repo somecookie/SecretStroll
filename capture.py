b3a335fd275da05c7a25b602118f90c904a3213b
somecookie <ricardo.ferreiraribeiro@protonmail.com>
2020-05-25 14:59:33 +0200

    Remove unncessary print

--- capture.py
+++ capture.py
@@ -19,7 +19,6 @@ def capture(host,iteration):
         os.mkdir(cell_path)    
         for i in range(iteration):
             cmd = f"tcpdump host {host} -i any -w {cell_path}/run{i}.pcap".split(" ")
-            print(cmd)
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
             os.system(f"python3 client.py grid -p key-client.pub -c attr.cred -r '' -t {cell_id}")
             time.sleep(1)
@@ -33,23 +32,10 @@ def get_args():
     parser.add_argument("--it", help="Number of iteration per cell", type=int, default=150)
     return parser.parse_args()
 
-# domains = ["www.google.com"]
-# for domain in domains:
-#     print("="*10,"Query to", domain,"="*10)
-    
-#     cmd = f"tcpdump host 192.168.1.20 -i any -w {domain}.pcap".split(" ")
-#     print(cmd)
-#     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-#     os.system(f"curl {domain}")
-#     time.sleep(1)
-#     p.send_signal(subprocess.signal.SIGTERM)
-#     time.sleep(1)
-
 if __name__ == "__main__":
     """
     For this script to work follow the same steps as in the README `A sample run of Part 3` replacing the last command of the client by:
     `python3 capture.py --ip <IP of the client> --it <number of iterations> `
     """
     args = get_args()
-    print(args)
     capture(args.ip, args.it)
